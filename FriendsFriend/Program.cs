134edb02407757fa4b5198ab129dc0bde76e1c85
Игорь Алиев <aliev.igor1@mail.ru>
2023-10-14 15:13:51 +0300

    gained Friend list

--- FriendsFriend/Program.cs
+++ FriendsFriend/Program.cs
@@ -1,9 +1,13 @@
 ﻿using CsvHelper;
 using CsvHelper.Configuration;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Runtime.InteropServices;
 using System.Text;
 using VkNet;
+using VkNet.Enums.Filters;
 using VkNet.Model;
+using VkNet.Utils;
 
 namespace FriendsFriend
 {
@@ -13,61 +17,64 @@ namespace FriendsFriend
         {
             string dataSetPath = @"data\IDList.csv";
             List<User> users = new List<User>();
-            var api = new VkApi();
+            VkApi api = new VkApi();
 
-            var config = new CsvConfiguration(CultureInfo.CurrentCulture) { Delimiter = ",", Encoding = Encoding.UTF8 };
 
-            using (var fileReader = File.OpenText(dataSetPath))
+            CsvConfiguration config = new CsvConfiguration(CultureInfo.CurrentCulture) { Delimiter = ",", Encoding = Encoding.UTF8 };
 
-            using (var csvResult = new CsvReader(fileReader, config))
+            using (StreamReader fileReader = File.OpenText(dataSetPath))
+
+            using (CsvReader csvResult = new CsvReader(fileReader, config))
             {
                 csvResult.Read();
                 csvResult.ReadHeader();
 
                 while (csvResult.Read())
                     users.Add(new User(csvResult.GetField<string>("Ваш ID в VK")));
-                
-            }
 
+                foreach (var user in users)
+                    user.SetID(user.CorrectVkId(user.Id));
+            }
 
             api.Authorize(new ApiAuthParams
             {
                 AccessToken = File.ReadAllText(@"data\VKToken.txt")
             });
 
-            var res = api.Groups.Get(new GroupsGetParams());
-
-            Console.WriteLine(res.TotalCount);
+            List<long> firstLayerUsersIDs = api.Users.Get(users.Select(n => n.Id)).Select(t => t.Id).ToList();
 
         }
+
+        class User
+        {
+            private string _id;
+            private string _name;
+            public VkCollection<User> friends;
+
+            public string Id { get { return _id; } }
+            public string Name { get { return _name; } }
+
+            public User()
+            {
+                _id = "NONE";
+                _name = "EMPTY";
+            }
+            public User(string id)
+            {
+                _id = id;
+                _name = "EMPTY";
+            }
+            public User(string id, string name)
+            {
+                _id = id;
+                _name = name;
+            }
+
+            public void SetName(string NewName) => _name = NewName;
+            public void SetID(string NewID) => _id = NewID;
+            public string CorrectVkId(string id) => id.Trim('@').Replace("https://vk.com/", "");
+
+        }
+
     }
-
-    class User
-    {
-        private string _id;
-        private string _name;
-        public string Id { get { return _id; } }
-        public string Name { get { return _name; } }
-        
-        public User()
-        {
-            _id = "NONE";
-            _name = "EMPTY";
-        }
-        public User(string id)
-        {
-            _id = id;
-            _name = "EMPTY";
-        }
-        public User(string id, string name)
-        {
-            _id = id;
-            _name = name;
-        }
-
-        public void SetName (string NewName) => _name = NewName;
-        public void SetID(string NewID) => _id = NewID;
-
-    }
-
 }
