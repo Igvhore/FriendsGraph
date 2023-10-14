07161b0db3743c5e92ef71bba1080985c3435905
ilya.marchevskiy <ilya.marchevskiy@mail.ru>
2023-10-14 20:32:50 +0300

    Дошел до 2го уровня, черновик записи в файл

--- FriendsFriend/Program.cs
+++ FriendsFriend/Program.cs
@@ -2,12 +2,15 @@
 using CsvHelper.Configuration;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
 using VkNet;
 using VkNet.Enums.Filters;
 using VkNet.Model;
 using VkNet.Utils;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 
 namespace FriendsFriend
 {
@@ -17,6 +20,8 @@ namespace FriendsFriend
         {
             string dataSetPath = @"data\IDList.csv";
             List<User> users = new List<User>();
+            List<User> usersFirstLayer = new List<User>();
+            List<User> usersSecondLayer = new List<User>();
             VkApi api = new VkApi();
 
 
@@ -35,13 +40,67 @@ namespace FriendsFriend
                 foreach (var user in users)
                     user.SetID(user.CorrectVkId(user.Id));
             }
-
+            
             api.Authorize(new ApiAuthParams
             {
                 AccessToken = File.ReadAllText(@"data\VKToken.txt")
             });
 
-            List<long> firstLayerUsersIDs = api.Users.Get(users.Select(n => n.Id)).Select(t => t.Id).ToList();
+            List<long> zeroLayerUsersIDs = api.Users.Get(users.Select(n => n.Id)).Select(t => t.Id).ToList();
+            File.AppendAllText(@"C:\Users\Илья\Desktop\0-1слой.txt", "Первый слой:");
+            File.AppendAllLines(@"C:\Users\Илья\Desktop\0-1слой.txt", zeroLayerUsersIDs.Select(n => Convert.ToString(n)));
+            File.AppendAllText(@"C:\Users\Илья\Desktop\0-1слой.txt", "Первый слой подробнее:");
+            int i_1 = 0;
+            foreach (var currentUser in users)
+            {
+                Cicle_1:
+                try
+                {
+                    currentUser.friends = api.Friends.Get(new FriendsGetParams { UserId = zeroLayerUsersIDs[i_1] }).Select(n => n.Id).ToList();
+                    Console.WriteLine("Чел с 0 уровня " + currentUser.Id + " дружит с " + currentUser.friends.Count);
+                    Thread.Sleep(1000);
+                    File.AppendAllText(@"C:\Users\Илья\Desktop\0-1слой.txt", zeroLayerUsersIDs[i_1].ToString());
+                   
+                    foreach (long friend in currentUser.friends)
+                    {
+                   
+                        usersFirstLayer.Add(new User(friend.ToString()));
+                        //File.AppendAllText(@"C:\Users\Илья\Desktop\0-1слой.txt", "Друзья:");
+                        //File.AppendAllLines(@"C:\Users\Илья\Desktop\0-1слой.txt", currentUser.friends.Select(n => Convert.ToString(n)));
+                    }
+                    i_1++;
+                }
+                catch
+                {
+                    i_1++;
+                    goto Cicle_1;
+                }
+                //api.Users.Get(new long[] { friend }).FirstOrDefault().FirstName + " " + api.Users.Get(new long[] { friend }).FirstOrDefault().LastName
+            }
+
+            int i_2 = 0;
+            
+            foreach (var currentUser in usersFirstLayer)
+            {
+                Cicle_2:
+                try
+                {
+                    currentUser.friends = api.Friends.Get(new FriendsGetParams { UserId = Convert.ToInt64(usersFirstLayer[i_2].Id) }).Select(n => n.Id).ToList();
+                    Console.WriteLine("Чел с 1го слоя " + currentUser.Id + " дружит с " + currentUser.friends.Count);
+                    Thread.Sleep(2500);
+                    foreach (long friend in currentUser.friends)
+                    {
+                        usersSecondLayer.Add(new User(friend.ToString()));
+                    }
+                    i_2++;
+                }
+                catch
+                {
+                    i_2++;
+                    goto Cicle_2;
+                }  
+            }
+
 
         }
 
@@ -49,7 +108,7 @@ namespace FriendsFriend
         {
             private string _id;
             private string _name;
-            public VkCollection<User> friends;
+            public List<long> friends = new List<long>();
 
             public string Id { get { return _id; } }
             public string Name { get { return _name; } }
