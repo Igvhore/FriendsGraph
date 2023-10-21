dd7482afa99415f6c7bedeb6598a519d427b20e3
ilya.marchevskiy <ilya.marchevskiy@mail.ru>
2023-10-21 18:01:01 +0300

    Запросы переписал на https

--- FriendsFriend/Program.cs
+++ FriendsFriend/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CsvHelper;
 using CsvHelper.Configuration;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -10,23 +11,25 @@ using VkNet.Enums.Filters;
 using VkNet.Model;
 using VkNet.Utils;
 using static System.Runtime.InteropServices.JavaScript.JSType;
-
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
 
 namespace FriendsFriend
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             string dataSetPath = @"data\IDList.csv";
             List<User> users = new List<User>();
             List<User> usersFirstLayer = new List<User>();
             List<User> usersSecondLayer = new List<User>();
             VkApi api = new VkApi();
-
+            string token1 = File.ReadAllText(@"C:\Users\Илья\Source\Repos\Igwhore\FriendsGraph\FriendsFriend\data\VKToken.txt");
 
             CsvConfiguration config = new CsvConfiguration(CultureInfo.CurrentCulture) { Delimiter = ",", Encoding = Encoding.UTF8 };
-
+          
             using (StreamReader fileReader = File.OpenText(dataSetPath))
 
             using (CsvReader csvResult = new CsvReader(fileReader, config))
@@ -40,69 +43,45 @@ namespace FriendsFriend
                 foreach (var user in users)
                     user.SetID(user.CorrectVkId(user.Id));
             }
-            
+            Console.WriteLine("Файл запарсен");
             api.Authorize(new ApiAuthParams
             {
-                AccessToken = File.ReadAllText(@"data\VKToken.txt")
+                AccessToken = File.ReadAllText(@"C:\Users\Илья\Source\Repos\Igwhore\FriendsGraph\FriendsFriend\data\VKToken.txt") //@"data\VKToken.txt"
             });
+            Console.WriteLine("Пользователь авторизован");
 
             List<long> zeroLayerUsersIDs = api.Users.Get(users.Select(n => n.Id)).Select(t => t.Id).ToList();
             File.AppendAllText(@"C:\Users\Илья\Desktop\0-1слой.txt", "Первый слой:");
             File.AppendAllLines(@"C:\Users\Илья\Desktop\0-1слой.txt", zeroLayerUsersIDs.Select(n => Convert.ToString(n)));
             File.AppendAllText(@"C:\Users\Илья\Desktop\0-1слой.txt", "Первый слой подробнее:");
-            int i_1 = 0;
-            foreach (var currentUser in users)
-            {
-                Cicle_1:
-                try
-                {
-                    currentUser.friends = api.Friends.Get(new FriendsGetParams { UserId = zeroLayerUsersIDs[i_1] }).Select(n => n.Id).ToList();
-                    Console.WriteLine("Чел с 0 уровня " + currentUser.Id + " дружит с " + currentUser.friends.Count);
-                    Thread.Sleep(1000);
-                    File.AppendAllText(@"C:\Users\Илья\Desktop\0-1слой.txt", zeroLayerUsersIDs[i_1].ToString());
-                   
-                    foreach (long friend in currentUser.friends)
-                    {
-                   
-                        usersFirstLayer.Add(new User(friend.ToString()));
-                        //File.AppendAllText(@"C:\Users\Илья\Desktop\0-1слой.txt", "Друзья:");
-                        //File.AppendAllLines(@"C:\Users\Илья\Desktop\0-1слой.txt", currentUser.friends.Select(n => Convert.ToString(n)));
-                    }
-                    i_1++;
-                }
-                catch
-                {
-                    i_1++;
-                    goto Cicle_1;
-                }
-                //api.Users.Get(new long[] { friend }).FirstOrDefault().FirstName + " " + api.Users.Get(new long[] { friend }).FirstOrDefault().LastName
-            }
 
-            int i_2 = 0;
-            
-            foreach (var currentUser in usersFirstLayer)
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            static async Task ProcessRepositoriesAsync(HttpClient client, string token1, long id)
             {
-                Cicle_2:
-                try
-                {
-                    currentUser.friends = api.Friends.Get(new FriendsGetParams { UserId = Convert.ToInt64(usersFirstLayer[i_2].Id) }).Select(n => n.Id).ToList();
-                    Console.WriteLine("Чел с 1го слоя " + currentUser.Id + " дружит с " + currentUser.friends.Count);
-                    Thread.Sleep(2500);
-                    foreach (long friend in currentUser.friends)
-                    {
-                        usersSecondLayer.Add(new User(friend.ToString()));
-                    }
-                    i_2++;
-                }
-                catch
-                {
-                    i_2++;
-                    goto Cicle_2;
-                }  
+                var json = await client.GetStringAsync(
+                    string.Format("https://api.vk.com/method/friends.get?user_id={0}&access_token={1}&v=5.131 HTTP/1.1", id, token1));
+                Console.WriteLine(json);
             }
 
 
+            foreach (var userId in zeroLayerUsersIDs)
+            {
+                Console.WriteLine(userId);
+
+                await ProcessRepositoriesAsync(client, token1, userId);
+                Thread.Sleep(100);
+                Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            }
+    
         }
+
+
+    }
 
         class User
         {
@@ -135,5 +114,4 @@ namespace FriendsFriend
 
         }
 
-    }
 }
